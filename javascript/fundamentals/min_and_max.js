bd4c4eb99b54fd31c9bda8ef0408b0f08caafac6
Vitor Müller <85710199+vitox013@users.noreply.github.com>
2022-05-11 23:00:04 -0300

    min and max

---
+++ javascript/fundamentals/min_and_max.js
@@ -0,0 +1,39 @@
+/*
+    Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers as input and return, respectively, the largest and lowest number in that list.
+    Examples (Input -> Output)
+
+    * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+    * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+    * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+    * [5]                             -> min = 5, max = 5
+
+    Notes
+
+    You may consider that there will not be any empty arrays/vectors.
+
+*/
+//my code
+var min = function(list){
+    let menor = list[0]
+    for(i = 0; i <= list.length;i++){
+        if(list[i] < menor)
+            menor = list[i]
+    }
+    return menor;
+}
+
+var max = function(list){
+    
+    let maior = list[0]
+    for(i = 0; i <= list.length;i++){
+        if(list[i] > maior)
+            maior = list[i]
+    }
+    return maior;
+}
+
+
+//best code on codewars
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
+
